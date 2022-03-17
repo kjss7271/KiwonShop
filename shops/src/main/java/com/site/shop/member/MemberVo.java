487bdaa77159a75fa5790823c53db29a22fa7da9
glwon <glwon@DESKTOP-EFRLPG3>
2022-03-17 14:21:56 +0900

    memberVo 래퍼런스 추가 / 회원가입 전용

--- shops/src/main/java/com/site/shop/member/MemberVo.java
+++ shops/src/main/java/com/site/shop/member/MemberVo.java
@@ -6,9 +6,153 @@ public class MemberVo {
 	private String m_id;
 	private String m_pw;
 	
+	private String m_idx;
+	private String m_name;
+	private String m_pw_do;
+	private String m_email;
+	private String m_email_status;
+	private String m_address;
+	private String m_address_detail;
+	private String m_phone_tel;
+	private String m_home_tel;
+	private String m_birth;
+	private String agree1;
+	private String agree2;
+	private String agree3;
+	private String business;
+	private String m_email_site;
+	
+	
+	
 	public MemberVo() {
 	}
 
+	
+	
+	
+	
+	
+	//회원가입
+	public String getM_idx() {
+		return m_idx;
+	}
+	public void setM_idx(String m_idx) {
+		this.m_idx = m_idx;
+	}
+	public String getM_name() {
+		return m_name;
+	}
+	public void setM_name(String m_name) {
+		this.m_name = m_name;
+	}
+	public String getM_pw_do() {
+		return m_pw_do;
+	}
+	public void setM_pw_do(String m_pw_do) {
+		this.m_pw_do = m_pw_do;
+	}
+	public String getM_email() {
+		return m_email;
+	}
+	public void setM_email(String m_email) {
+		this.m_email = m_email;
+	}
+	public String getM_email_status() {
+		return m_email_status;
+	}
+	public void setM_email_status(String m_email_status) {
+		this.m_email_status = m_email_status;
+	}
+	public String getM_address() {
+		return m_address;
+	}
+	public void setM_address(String m_address) {
+		this.m_address = m_address;
+	}
+	public String getM_address_detail() {
+		return m_address_detail;
+	}
+	public void setM_address_detail(String m_address_detail) {
+		this.m_address_detail = m_address_detail;
+	}
+	public String getM_phone_tel() {
+		return m_phone_tel;
+	}
+	public void setM_phone_tel(String m_phone_tel) {
+		this.m_phone_tel = m_phone_tel;
+	}
+	public String getM_home_tel() {
+		return m_home_tel;
+	}
+	public void setM_home_tel(String m_home_tel) {
+		this.m_home_tel = m_home_tel;
+	}
+	public String getM_birth() {
+		return m_birth;
+	}
+	public void setM_birth(String m_birth) {
+		this.m_birth = m_birth;
+	}
+	public String getAgree1() {
+		return agree1;
+	}
+	public void setAgree1(String agree1) {
+		this.agree1 = agree1;
+	}
+	public String getAgree2() {
+		return agree2;
+	}
+	public void setAgree2(String agree2) {
+		this.agree2 = agree2;
+	}
+	public String getAgree3() {
+		return agree3;
+	}
+	public void setAgree3(String agree3) {
+		this.agree3 = agree3;
+	}
+	public String getBusiness() {
+		return business;
+	}
+	public void setBusiness(String business) {
+		this.business = business;
+	}
+	public String getM_email_site() {
+		return m_email_site;
+	}
+	public void setM_email_site(String m_email_site) {
+		this.m_email_site = m_email_site;
+	}
+	//회원가입
+	public MemberVo(String m_id, String m_pw, String m_idx, String m_name, String m_pw_do, String m_email,
+			String m_email_status, String m_address, String m_address_detail, String m_phone_tel, String m_home_tel,
+			String m_birth, String agree1, String agree2, String agree3, String business, String m_email_site) {
+		super();
+		this.m_id = m_id;
+		this.m_pw = m_pw;
+		this.m_idx = m_idx;
+		this.m_name = m_name;
+		this.m_pw_do = m_pw_do;
+		this.m_email = m_email;
+		this.m_email_status = m_email_status;
+		this.m_address = m_address;
+		this.m_address_detail = m_address_detail;
+		this.m_phone_tel = m_phone_tel;
+		this.m_home_tel = m_home_tel;
+		this.m_birth = m_birth;
+		this.agree1 = agree1;
+		this.agree2 = agree2;
+		this.agree3 = agree3;
+		this.business = business;
+		this.m_email_site = m_email_site;
+	}
+
+
+
+
+
+
+	//로그인
 	public String getM_id() {
 		return m_id;
 	}
@@ -24,7 +168,7 @@ public class MemberVo {
 	public void setM_pw(String m_pw) {
 		this.m_pw = m_pw;
 	}
-
+	//로그인
 	public MemberVo(String m_id, String m_pw) {
 		super();
 		this.m_id = m_id;
